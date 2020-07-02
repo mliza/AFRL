26b75cee02661194766ff7e2bd5712d9e4e05638
Martin E. Liza <mliza@email.arizona.edu>
2020-07-01 22:41:16 -0700

    Fixed OPL, added several plots and fixed tecParser.py to allow output data name

--- summer2020/runs/calculateOPL.m
+++ summer2020/runs/calculateOPL.m
@@ -15,13 +15,23 @@
    Martin E. Liza      06/21/2020      Revised the script to work with 
                                        multiple dimension structures, 
                                        implemented OPL and OPD.
+   Martin E. Liza      07/01/2020      Revised the Script to work with the
+                                       sliced data from tecplot. OPL, OPD and
+                                       index of Reraction plots were added.
 %}
 
 clear all; close all; clc; 
 % Import Data
-dataIn = dataParser('dataOut.dat');
+dataIn = dataParser('outData01.dat');
+numIn = 0.10; 
 [ constGD, neutrGD, ionGD ] = constantsGD(); 
 [ rowIn, colIn ] = size(dataIn.X);
+
+% Create title template 
+dyCut = sprintf('y = %s[m],', num2str(numIn));
+titleName = strcat(dyCut, ' AoA = 45^{\circ}');
+saveTitle = strrep(num2str(numIn), '.', '');
+pathToSave = '/Users/Martin/Desktop/MDA/figures';
 
 
 % Calculate total Gladsone-Dale Constant and index of refraction 
@@ -37,7 +47,11 @@ totIonGD   = ( (ionGD.O .* dataIn.rho_O) + (ionGD.O2 .* dataIn.rho_O2) + ...
                (ionGD.N .* dataIn.rho_N) + (ionGD.NO .* dataIn.rho_NO) + ...
                (ionGD.N2 .* dataIn.rho_N2) ) ./ dataIn.rho; 
 
-N.constant = totConstGD .* dataIn.rho; 
+GD.tables  = totConstGD;  
+GD.neutral = totNeutrGD;
+GD.ion     = totIonGD;
+
+N.tables   = totConstGD .* dataIn.rho; 
 N.neutral  = totNeutrGD .* dataIn.rho;
 N.ion      = totIonGD .* dataIn.rho;
 
@@ -47,21 +61,56 @@ for n = 1:length(nFieldName)
     headerName = nFieldName{n};
     indexN = N.(headerName);
     for i = 1:colIn 
-        vectorOPL(i) = trapz(dataIn.Y(:,1),indexN(:,i)); 
-        OPL.(headerName) = vectorOPL; 
+        OPL.(headerName) = dataIn.X .* indexN; 
         OPD.(headerName) = OPL.(headerName) - mean(OPL.(headerName));
     end
 end
 
-% Plot OPD  
+% Plot Index of Refraction  
 figure 
 for i = 1:length(nFieldName)
     nIndx = nFieldName{i}; 
-    plot(dataIn.X(1,:), OPD.(nIndx), 'o-')
+    plot(dataIn.X, N.(nIndx), '-')
     hold on 
 end 
 legend(nFieldName);
-xlabel('distance-X [m]');
-ylabel('OPD [m]');
+title(titleName, 'Interpreter', 'tex');
+xlabel('distance-x [m]', 'Interpreter', 'tex');
+ylabel('(n-1) [ ]','Interpreter', 'tex');
+set(gcf, 'InvertHardcopy', 'off');
+xlim([0 0.5]);
 hold off
+%saveas(gcf, sprintf('%s/indexN_%s.png', pathToSave, saveTitle))
+
+% Plot OPL 
+figure 
+for i = 1:length(nFieldName)
+    nIndx = nFieldName{i}; 
+    plot(dataIn.X, OPL.(nIndx), '-')
+    hold on 
+end 
+legend(nFieldName, 'Location', 'southeast');
+title(titleName, 'Interpreter', 'tex');
+xlabel('distance-x [m]', 'Interpreter', 'tex');
+ylabel('(OPL - 1) [m]','Interpreter', 'tex');
+set(gcf, 'InvertHardcopy', 'off');
+xlim([0 0.5]);
+hold off
+%saveas(gcf, sprintf('%s/OPL_%s.png', pathToSave, saveTitle))
+
+% Plot OPD 
+figure 
+for i = 1:length(nFieldName)
+    nIndx = nFieldName{i}; 
+    plot(dataIn.X, OPD.(nIndx), '-')
+    hold on 
+end 
+legend(nFieldName, 'Location', 'southeast');
+title(titleName, 'Interpreter', 'tex');
+xlabel('distance-x [m]', 'Interpreter', 'tex');
+ylabel('OPD [m]','Interpreter', 'tex');
+set(gcf, 'InvertHardcopy', 'off');
+xlim([0 0.5]);
+hold off
+%saveas(gcf, sprintf('%s/OPD_%s.png', pathToSave, saveTitle))
 
--- summer2020/runs/tecParser.py
+++ summer2020/runs/tecParser.py
@@ -7,11 +7,11 @@ from tecplot.constant import *
 # Uncomment the following line to connect to a running instance of Tecplot 360:
 # tp.session.connect()
 
+# runint ./tecParser.py 'inputFile/output.pl' 0.4 
 argIn = sys.argv 
 pltIn = argIn[1] 
 dy = argIn[2]
-
-  #VarNameList = '\"X\" \"Y\" \"rho_N2\" \"rho_O2\" \"rho_NO\" \"rho_N\" \"rho_O\" \"rho_NOp\" \"rho_N2p\" \"rho_O2p\" \"rho_Np\" \"rho_Op\" \"rho_e\" \"U\" \"V\" \"T\" \"Tv\" \"rho\" \"P\" \"H\" \"tau\"'""")
+dataOut = argIn[3]
 
 tp.macro.execute_command(f"""$!ReadDataSet '\"{pltIn}\"'
   ReadDataOption = New
@@ -26,6 +26,6 @@ tp.macro.execute_command('''$!Pick AddAtPosition
 tp.macro.execute_command('''$!FrameControl ActivateByNumber
   Frame = 1''')
 tp.macro.execute_extended_command(command_processor_id='Extract Precise Line',
-    command=f"XSTART = 0 YSTART = {dy} ZSTART = 0 XEND = 2.5 YEND = {dy} ZEND = 0 NUMPTS = 1000 EXTRACTTHROUGHVOLUME = F EXTRACTTOFILE = T EXTRACTFILENAME = 'dataOut.dat' ")
+    command=f"XSTART = 0 YSTART = {dy} ZSTART = 0 XEND = 0.5 YEND = {dy} ZEND = 0 NUMPTS = 1000 EXTRACTTHROUGHVOLUME = F EXTRACTTOFILE = T EXTRACTFILENAME = '{dataOut}.dat' ")
 # End Macro.
 
